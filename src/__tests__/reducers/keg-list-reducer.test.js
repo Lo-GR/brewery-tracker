5c5e175ed25e9840971dd80bdcfcabcbf4fda4ba
Lo-GR <diamondintheroth@gmail.com>
2021-04-30 09:54:27 -0700

    pass good fail for first test and WIP second test

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,23 @@
 import kegListReducer from "../../reducers/keg-list-reducer";
 
 describe('kegListReducer', () => {
+  let action;
+  const kegData = {
+    name: 'Green Beer',
+    id: 1,
+    flavor: "IPA",
+    alc: 5.5,
+    price: 4.4,
+    brand: "GreenWay Beer"
+  }
   test('should return default state if there is no action type passed', () =>{
     expect(kegListReducer({}, {type: null})).toEqual({});
   });
+
+  test('Should successfully add new keg data to masterKegList', ()=>{
+    const {name, id, flavor, alc, price, brand} = kegData;
+    action = {
+      
+    }
+  })
 });

dbc9c9f6be69eb1f4b23683eb7f9fc248fae1a98
Lo-GR <diamondintheroth@gmail.com>
2021-04-30 13:58:30 -0700

    write test and pass good fail for toggle to true

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -1,7 +1,11 @@
 import formVisibleReducer from "../../reducers/form-visible-reducer";
+import * as c from "../../actions/ActionTypes";
 
 describe('formVisibleReducer', ()=>{
   test('should return default state if no action', ()=>{
     expect(formVisibleReducer(false, { type: null})).toEqual(false);
+  })
+  test('should toggle form visibility state to true', ()=>{
+    expect(formVisibleReducer(false, c.TOGGLE_FORM)).toEqual(true);
   })
 })

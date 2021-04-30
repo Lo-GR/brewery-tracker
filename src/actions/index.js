3fe4da3e55d189f562d4378883598571cab1772d
Lo-GR <diamondintheroth@gmail.com>
2021-04-30 11:24:44 -0700

    finish addKeg and pass test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,14 @@
 import * as c from "./ActionTypes";
 
 export const addKeg = kegData => {
-  // const {name, id, flavor, alc, price, brand} = kegData;
-
-  // return {
-  //   type: c.ADD_KEG,
-  //   name: name,
-  //   id: id,
-  //   flavor: flavor,
-  //   alc: alc,
-  //   price: price,
-  //   brand: brand
-  // };
-  return undefined;
+  const {name, id, flavor, alc, price, brand} = kegData;
+  return {
+    type: c.ADD_KEG,
+    name: name,
+    id: id,
+    flavor: flavor,
+    alc: alc,
+    price: price,
+    brand: brand
+  };
 }

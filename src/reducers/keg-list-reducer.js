410d7f912de686ce5dd57c3a2279dc3506650d1d
Lo-GR <diamondintheroth@gmail.com>
2021-04-30 10:13:34 -0700

    create add keg reducer and pass second test

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,19 @@
+import * as c from "../actions/ActionTypes";
+
 export default (state = {}, action) => {
+  const {name, id, flavor, alc, price, brand} = action;
+  switch (action.type){
+    case c.ADD_KEG:
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          id: id,
+          flavor: flavor,
+          alc: alc,
+          price: price,
+          brand: brand
+        }
+      })
+  }
   return state;
 }

ab1a91d539c6af2e24d50e02fa8976d2a4bdd449
Lo-GR <diamondintheroth@gmail.com>
2021-04-30 09:38:23 -0700

    fix tests and pass good fail

--- src/__tests__/actions/reducers/keg-list-reducer.test.js
+++
@@ -1,7 +0,0 @@
-import kegListReducer from "../../reducers/keg-list-reducer";
-
-describe('kegListReducer', () => {
-    test('should return default state if there is no action type passed', () =>{
-      expect(kegListReducer({}, {type: null})).toEqual({});
-    });
-  });

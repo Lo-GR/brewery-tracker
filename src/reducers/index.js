77839b1883ab3c301ccdc49ca0267926b8609ec6
Lo-GR <diamondintheroth@gmail.com>
2021-04-30 16:26:24 -0700

    add selectedkeg to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import formVisibleReducer from "./form-visible-reducer";
 import kegListReducer from "./keg-list-reducer";
+import selectedKegReducer from "./selected-keg";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   formVisible: formVisibleReducer,
-  masterKegList: kegListReducer
+  masterKegList: kegListReducer,
+  selectedKeg: selectedKegReducer
 })
 
 export default rootReducer;

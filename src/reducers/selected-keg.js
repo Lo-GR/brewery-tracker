69d7c077a9ae704a5fb127b94461f09bbfc94055
Lo-GR <diamondintheroth@gmail.com>
2021-04-30 17:53:21 -0700

    pass test for selectkeg reducer

--- src/reducers/selected-keg.js
+++ src/reducers/selected-keg.js
@@ -3,6 +3,8 @@ import * as c from "../actions/ActionTypes";
 export default (state = "", action) =>{
   switch (action.type){
   case c.SELECT_KEG:
-  default: return state;
+    return action.id;
+  default: 
+    return state;
   }
 }
